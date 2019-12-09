b90c986635d983135724d42c21ca7e26dbbae449
mkrill <michael.krill@telekom.de>
2019-12-09 12:57:08 +0100

    content coding part completed

--- src/main/java/de/telekom/Spring02Ontheroad/Spring02OnTheRoadApplication.java
+++ src/main/java/de/telekom/Spring02Ontheroad/Spring02OnTheRoadApplication.java
@@ -2,7 +2,13 @@ package de.telekom.Spring02Ontheroad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class Spring02OnTheRoadApplication {
 
@@ -10,4 +16,13 @@ public class Spring02OnTheRoadApplication {
 		SpringApplication.run(Spring02OnTheRoadApplication.class, args);
 	}
 
+	@RequestMapping("/hello/{name}")
+	@ResponseBody
+	public String hello(@PathVariable("name") String name, @RequestParam(defaultValue = "") String title) {
+		if (title.length() > 0) {
+			return "Hello " + name + ", " + title;
+		} else {
+			return "Hello " + name;
+		}
+	}
 }
