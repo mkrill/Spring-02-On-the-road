30676fcb216b4203a823772e3e113f2f9d344ab8
mkrill <michael.krill@telekom.de>
2019-12-09 12:24:49 +0100

    first commit before devstart

---
+++ src/main/java/de/telekom/Spring02Ontheroad/Spring02OnTheRoadApplication.java
@@ -0,0 +1,13 @@
+package de.telekom.Spring02Ontheroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring02OnTheRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring02OnTheRoadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/telekom/Spring02Ontheroad/Spring02OnTheRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telekom.Spring02Ontheroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring02OnTheRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
